0645c9955637036cb2e3bc6970e0235155c55176
Jonathan Dickinson <dickinsonjo@Jonathans-MBP.lan>
2022-04-10 10:44:04 -0700

    Added an expander bar, along with plotly histogram and key insights

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import streamlit as st
 import pandas as pd
 import plotly as py
 import plotly.graph_objects as go
+import plotly.express as px
 
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
@@ -39,75 +40,121 @@ with st.container():
     with right_column:
         st.markdown("![Alt Text](https://i.pinimg.com/originals/e4/26/70/e426702edf874b181aced1e2fa5c6cde.gif)")
 
+
 # --- Projects ---
 with st.container():
     st.write("---")
     st.header("My Projects")
     st.write("##")
 
-image_column, text_column = st.columns((1,2))
-with image_column:
-    st.image("https://cdn11.bigcommerce.com/s-jyvxk5hzsq/images/stencil/500x659/products/8329/46012/8759square__99323.1604697767.jpg?c=2")
-with text_column:
-    st.header("College Cost Analysis")
+    # --- Drop down to hide projects ---
+    with st.expander("College Cost Analysis"):
 
-# --- Drop down to hide this specific project ---
+        image_column, text_column = st.columns((1,2))
+        with image_column:
+            st.image("https://cdn11.bigcommerce.com/s-jyvxk5hzsq/images/stencil/500x659/products/8329/46012/8759square__99323.1604697767.jpg?c=2")
+        with text_column:
+            st.header("College Cost Analysis")
 
-    st.subheader("Description:")
-    st.write(
-        '''
-        The U.S. Department of Education provides the most reliable data on college costs, graduation, and 
-        post-graduation earnings. The purpose of the College Scorecard project is to increase transparancy and awareness
-        on the true cost of higher education so that students and families can make informed decisions about college.
-        Personally, I believe these data are very valuable and should be widely accessible, however the highly dimensional
-        nature of the dataset is not very userfriendly. My goal is to help make this dataset more accessible to those looking
-        to continue their education in postsecondary education.
+# --------------------------------- Description of project -------------------------------
 
-        If you would like to look further into the data documentation, or how these visualization were created, click the
-        link to my GitHub at the top of the web page.
+            st.subheader("Description:")
+            st.write(
+            '''
+            The U.S. Department of Education provides the most reliable data on college costs, graduation, and 
+            post-graduation earnings. The purpose of the College Scorecard project is to increase transparancy and awareness
+            on the true cost of higher education so that students and families can make informed decisions about college.
+            Personally, I believe these data are very valuable and should be widely accessible, however the highly dimensional
+            nature of the dataset is not very userfriendly. My goal is to help make this dataset more accessible to those looking
+            to continue their education in postsecondary education.
 
-        These visualizations will be continually updated as the analysis progresses.
-        '''
-    )
-st.subheader("Median Cost of Attendance Map")
+            If you would like to look further into the data documentation, or how this analysis was conducted, click the
+            link to my GitHub at the top of the web page.
 
-#---Insert Plotly graph ---
-df = pd.read_csv('https://raw.githubusercontent.com/jonathjd/webapp/main/state_df.csv', index_col='STABBR')
-df_display = pd.read_csv('https://raw.githubusercontent.com/jonathjd/Education-Project/main/data/processed/web_app_data.csv')
-
-# Create dictionary with data
-# This map will display median state income
-data = dict(type='choropleth',
-            locations = df.index,
-            locationmode='USA-states',
-            z=df['COSTT4_A'],
-            colorscale="Reds",
-            colorbar_title="USD"
+            These visualizations will be continually updated as the analysis progresses.
+            '''
             )
 
-# configure layout
-layout = dict(geo=dict(
-    scope='usa',
-    showlakes=True,
-    lakecolor="rgb(85,173,240)")
-             )
+        st.write("---")
 
-#---Display Plotly Graph--
-with st.container():
-    fig = go.Figure(data=[data], layout=layout)
-    st.plotly_chart(fig)
-    st.write('''
-             The figure above is an interative map that allows you to hover over the state of interest and observe the median
-             value of the average cost of attendance for each state. The The average annual total cost of attendance, including 
-             tuition and fees, books and supplies, and living expenses, minus the average grant/scholarship aid, by detailed income category. 
-             It is calculated for all full-time, first-time, degree/certificate-seeking undergraduates who receive Title IV aid.
+# ------------------------------------- Quick insights to project -------------------------------------------------------------
+        st.header("Quick insights")
+        st.write(
+        '''
+        - The cost of a attendance for one year at a **junior college**  in the US is **$14000** USD.
+        - The cost of a attendance for one year at a **primarily undergraduate institution** in the is **$25000** USD.
+        - The cost of a attendance for one year at an institution that **primarily awards graduate degrees** is **$29000** USD.
+        - Institutions that are private, primarily award graduate degrees (masters, doctorates), and are located in the northeast U.S. are the most expensive on average.
+        - More than a quarter of all the schools in the U.S. that have an annual cost between **10000** and **20000** USD are located in the **southeast region** (AL, AR, FL, GA, KY, LA, MS, SC, TN, VA, WV).
+        - **3 out of 4** schools in the U.S. that have an annual cost of attendance between **10000** and **20000** USD are public schools.
+        '''
+        )
+        st.subheader("Key Takeaways")
+        st.write('''
+        To minimze the cost of attending a 4-year institution in the U.S., attend a public university that is located in the southeast U.S.
+        This analysis did not factor in future earnings, for those data checkout Georgetown Universities interactive web tool. 
+        ''')
+        st.write("[Georgetown Interactive Web Tool>](https://cew.georgetown.edu/cew-reports/college-rankings/)")
+        st.write("---")
+
+        # ------------------------------------- Key Visualizations -------------------------------------------------------------
+        # Insert dataframes
+        df = pd.read_csv('https://raw.githubusercontent.com/jonathjd/webapp/main/state_df.csv', index_col='STABBR')
+        df_display = pd.read_csv('https://raw.githubusercontent.com/jonathjd/Education-Project/main/data/processed/web_app_data.csv')
+        
+        # U.S. Histogram
+        with st.container():
+            st.subheader("Cost of Attendance Histogram")
+
+            #Plotly figure object
+            fig = px.histogram(df_display, x="Cost of Attendance",
+                labels ={
+                "count": "Number of Universities"
+            },
+            template="simple_white" 
+            )
             
-            The median value of the average cost of attendance for every institution that primarily awards either graduate
-            or bachelors degree is shown.
-             ''')
+            st.plotly_chart(fig, use_container_width=True)
 
-#--- Display dataframe---
-with st.container():
-    st.subheader("Institutional Data")
-    st.write(df_display)
+        # ------------------------------------- Plotly Map -------------------------------------------------------------
+
+
+        st.subheader("Median Cost of Attendance Map")
+
+        #---Insert Plotly graph ---
+        # Create dictionary with data
+        # This map will display median state income
+        data = dict(type='choropleth',
+                locations = df.index,
+                locationmode='USA-states',
+                z=df['COSTT4_A'],
+                colorscale="Reds",
+                colorbar_title="USD"
+                )
+
+        # configure layout
+        layout = dict(geo=dict(
+            scope='usa',
+            showlakes=True,
+            lakecolor="rgb(85,173,240)")
+                )
+
+    #---Display Plotly Graph--
+        with st.container():
+            fig = go.Figure(data=[data], layout=layout)
+            st.plotly_chart(fig, use_container_width=True)
+            st.write('''
+                The figure above is an interative map that allows you to hover over the state of interest and observe the median
+                value of the average cost of attendance for each state. The The average annual total cost of attendance, including 
+                tuition and fees, books and supplies, and living expenses, minus the average grant/scholarship aid, by detailed income category. 
+                It is calculated for all full-time, first-time, degree/certificate-seeking undergraduates who receive Title IV aid.
+                
+                The median value of the average cost of attendance for every institution that primarily awards either graduate
+                or bachelors degree is shown.
+                ''')
+
+        #--- Display dataframe---
+        with st.container():
+            st.subheader("Institutional Data")
+            st.write(df_display)
         
